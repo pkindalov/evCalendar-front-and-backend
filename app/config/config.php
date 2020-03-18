9e9aaacf3f37ef09674e318fdcff6e81f0a12c33
pkindalov <panayotkindalov@gmail.com>
2020-03-18 22:48:54 +0200

    edited conf files

--- app/config/config.php
+++ app/config/config.php
@@ -3,13 +3,13 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', '');
-    define('DB_NAME', 'shareposts');
+    define('DB_NAME', 'evCalendar');
 
     //App root
     define('APPROOT', dirname(dirname(__FILE__)));
 
     //URL root
-    define('URLROOT', 'http://localhost/shareposts'); 
+    define('URLROOT', 'http://localhost/evCalendar'); 
 
     //Site name
-    define('SITENAME', 'SharePosts');
+    define('SITENAME', 'evCalendar');
