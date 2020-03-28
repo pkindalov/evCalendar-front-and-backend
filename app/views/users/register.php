b37c2da37adedee218b7484e6f58f3e9acc4d8cb
pkindalov <panayotkindalov@gmail.com>
2020-03-28 17:27:00 +0200

    Design updates

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
   <div class="row">
-    <div class="col-md-6 mx-auto">
-      <div class="card card-body bg-light mt-5">
+    <div class="col s12">
+      <div class="card card-body bg-light center">
         <h2>Create An Account</h2>
         <p>Please fill out this form to register with us</p>
         <form action="<?php echo URLROOT; ?>/users/register" method="post">
@@ -24,15 +24,8 @@
             <label for="confirm_password">Confirm Password: <sup>*</sup></label>
             <input type="password" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['confirm_password']; ?>">
             <span class="invalid-feedback"><?php echo $data['confirm_password_err']; ?></span>
-          </div>
-
-          <div class="row">
-            <div class="col">
-              <input type="submit" value="Register" class="btn btn-success btn-block">
-            </div>
-            <div class="col">
-              <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-light btn-block">Have an account? Login</a>
-            </div>
+            <input type="submit" value="Register" class="btn btn-success">
+            <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-light">Have an account? Login</a>
           </div>
         </form>
       </div>
